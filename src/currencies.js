db465742e50bddf20869f26ac1acec5aeb1444c5
Grzegorz Domin <grze.domin98@gmail.com>
2022-03-31 12:33:14 +0200

    created array with currencies

---
+++ src/currencies.js
@@ -0,0 +1,22 @@
+const currencies = [
+    {
+        name: "Dolar Amerykański",
+        short: "USD",
+        rate: 4.10,
+        id: 1,
+    },
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.45,
+        id: 2,
+    },
+    {
+        name: "Funt Brytyjski",
+        short: "GBP",
+        rate: 5,
+        id: 3,
+    },
+];
+
+export default currencies;
