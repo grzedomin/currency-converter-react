fd4adcf627f529574ef7a3b19dcaf34aeacebe7e
Grzegorz Domin <grze.domin98@gmail.com>
2022-04-01 09:51:08 +0200

    code formatted

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import "./style.css";
 
 const Buttons = ({ resetContent }) => (
-
-    <React.Fragment>
+    <>
         <button className="form__button">Zamień</button>
         <button
             onClick={() => resetContent()}
-            className="form__button">Reset</button>
-        </React.Fragment >   
+            className="form__button"
+        >
+            Reset
+        </button>
+    </>
 );
 
 export default Buttons;
