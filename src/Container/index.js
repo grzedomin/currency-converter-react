bdb01ddc934e720376eec767815ddb2a73390759
Grzegorz Domin <grze.domin98@gmail.com>
2022-04-06 11:47:20 +0200

    Container styled with styled-components

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,8 +1,8 @@
-import "./style.css";
+import { ContainerArea } from "./styled";
 
 const Container = ({ children }) => (
-    <div className="container">
+    <ContainerArea>
         {children}
-    </div>
+    </ContainerArea>
 );
 export default Container;
---
+++ src/Container/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContainerArea = styled.div`
+    max-width: 650px;
+    width: 100%;
+    margin: auto;
+`;
