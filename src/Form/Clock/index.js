2465179b0eace2fb351836c3977a46dede912868
Grzegorz Domin <grze.domin98@gmail.com>
2022-04-08 19:40:20 +0200

    created formatDate function

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,19 +2,22 @@ import { useCurrentDate } from "./useCurrentDate";
 import { DateInfo } from "./styled";
 
 export const Clock = () => {
-
+    
     const date = useCurrentDate();
 
+    const formatDate = () => date.toLocaleDateString("pl", {
+        weekday: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
     return (
-        <DateInfo>{`Dzisiaj jest ${date.toLocaleDateString(undefined, {
-            weekday: "long",
-            hour: "2-digit",
-            minute: "2-digit",
-            secod: "2-digit",
-            day: "numeric",
-            month: "long",
-            year: "numeric"
-        })}`}
+        <DateInfo>
+            Dzisiaj jest {""} {formatDate(date)}
         </DateInfo>
     );
 };
